b838e7d333a8e7d37a8d5dc9d65f068a68d66c31
wasikkamila <wasikkamila3@gmail.com>
2025-11-26 19:21:21 +0100

    Update results.js

--- results.js
+++ results.js
@@ -2,3 +2,17 @@ window.addEventListener("DOMContentLoaded", () => { //display score on the resul
     const finalScore = localStorage.getItem("quizScore");
     document.getElementById("score").textContent = finalScore;
 })
+
+document.getElementById("playAgain").addEventListener("mouseover", function() {
+    document.getElementById("playAgain").src = "images/playagainD.png";
+});
+
+document.getElementById("playAgain").addEventListener("mouseout", function() {
+    document.getElementById("playAgain").src = "images/playagain.png";
+});
+
+document.getElementById("playAgain").addEventListener("click", function() {
+    localStorage.removeItem('quizScore');
+    window.location.href="index.html";
+});
+
